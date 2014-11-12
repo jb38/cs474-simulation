40385224da884270e981768730d96cb533fe515d
Jim Blaney <jb38@hood.edu>
2014-11-11 23:00:25 -0500

    removed a bunch of extra stuff

--- src/main/java/edu/hood/cs/sim/Simulation.java
+++ src/main/java/edu/hood/cs/sim/Simulation.java
@@ -1,22 +1,12 @@
 package edu.hood.cs.sim;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
 import sim.engine.SimState;
-import sim.field.network.Network;
-import util.CsvParser;
-import edu.hood.cs.sim.domain.Airport;
+import edu.hood.cs.sim.repositories.AircraftRepository;
+import edu.hood.cs.sim.util.HibernateUtil;
 
 @SuppressWarnings("serial")
 public class Simulation extends SimState {
-	
-	private Network network = new Network(true);
-	
-	private static final String AIRPORTS_FILE = "airports.csv";
-	
+		
 	public Simulation(long seed) {
 		super(seed);
 	}
@@ -25,24 +15,19 @@ public class Simulation extends SimState {
 	public void start() {
 		super.start();
 		
-		try {
-			Properties p = new Properties();
-			p.load(getClass().getResourceAsStream(AIRPORTS_FILE));
-			
-			
-		} catch (IOException ioe) {
-			
-		}
-		//network.addNode(node); // TODO add a node for each airport
+		System.out.println(AircraftRepository.getInstance().fetchAll().size());
 	}
 	
 	@Override
 	public void finish() {
 		super.finish();
+		
+		HibernateUtil.getSessionFactory().close();
 	}
 
 	public static void main(String[] args) {
 		doLoop(Simulation.class, args);
+		
 		System.exit(0);
 	}
 }
--- src/main/java/edu/hood/cs/sim/domain/Aircraft.java
+++ src/main/java/edu/hood/cs/sim/domain/Aircraft.java
@@ -1,10 +1,57 @@
 package edu.hood.cs.sim.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
 import edu.hood.cs.sim.Simulation;
 import sim.engine.*;
 
-@SuppressWarnings("serial")
+@Entity
+@Table(name="AIRCRAFT")
 public class Aircraft implements Steppable {
+
+	private int id;
+	private String carrier;
+	private String tailNum;
+	
+	public Aircraft() {
+		
+	}
+	
+	public Aircraft(int id, String carrier, String tailNum) {
+		this.id = id;
+		this.carrier = carrier;
+		this.tailNum = tailNum;
+	}
+	
+	@Id
+    public Integer getId() {
+        return this.id;
+    }
+ 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+	
+    @Column(name="CARRIER")
+	public String getCarrier() {
+		return carrier;
+	}
+
+	public void setCarrier(String carrier) {
+		this.carrier = carrier;
+	}
+
+	@Column(name="TAIL_NUM")
+	public String getTailNum() {
+		return tailNum;
+	}
+
+	public void setTailNum(String tailNum) {
+		this.tailNum = tailNum;
+	}
 
 	public void step(SimState state) {
 		Simulation sim = (Simulation)state;

38f650d2895d6ee5399f08ad9cc3d6908b70f6e6
David Ford <macco88@gmail.com>
2014-11-30 01:57:13 -0500

    Small update
    Will send in a more substantial update later in the day…just added the
    beginnings to Instrumentation for file writing, etc. Should discuss
    again how we will calculate number of impacted flights in Aircraft so
    that can be passed along.

--- src/main/java/edu/hood/cs/sim/Instrumentation.java
+++ src/main/java/edu/hood/cs/sim/Instrumentation.java
@@ -1,8 +1,25 @@
+// This class writes pipe-delimited output to a text file.
+// The content written comes from the Aircraft class, which
+// is used to generate delays at random for the flights, and 
+// then will give us a number for total impacted flights.
+
 package edu.hood.cs.sim;
+
+import edu.hood.cs.sim.domain.Aircraft;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
 public class Instrumentation {
 
 	private static final Instrumentation instance = new Instrumentation();
+	
+	private int numImpactedFlights = 0;
+	private double delay = 0;
+	
+	Aircraft aircraft = new Aircraft();
+	
+	
 	
 	public static Instrumentation getInstance() {
 		return instance;
@@ -12,8 +29,19 @@ public class Instrumentation {
 		
 	}
 	
+	// TODO
+			// will spit out into text file (pipes, tabs, comma...whatever delimited)
+			// the delays of each flight. should have year/month/day/hour/second of when the 
+			// class is first opened/created (singleton...only one instance globally for this class)
+			// static method getInstance...
+			// delay and the number of flights impacted are what will get pushed to text file (one entry per run)
+			// 
+	
 	public void registerDelay(double delay, int numImpactedFlights) {
-		// TODO
+		delay = aircraft.getDelay(); 
+		this.numImpactedFlights = numImpactedFlights;
 	}
+	
+	
 	
 }

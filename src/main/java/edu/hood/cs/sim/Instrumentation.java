ffe2faf5c5028c63cc55d5cf4265b32b214dceab
Jim Blaney <jb38@hood.edu>
2014-11-30 09:15:38 -0500

    fixed implementation

--- src/main/java/edu/hood/cs/sim/Instrumentation.java
+++ src/main/java/edu/hood/cs/sim/Instrumentation.java
@@ -1,46 +1,56 @@
-// This class writes pipe-delimited output to a text file.
-// The content written comes from the Aircraft class, which
-// is used to generate delays at random for the flights, and 
-// then will give us a number for total impacted flights.
-
 package edu.hood.cs.sim;
 
-import java.io.File;
-
-import edu.hood.cs.sim.domain.Aircraft;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Instrumentation {
 
+	// TODO maybe create a domain POJO and store the values in the database
+	
+	public static final String DELIMITER = "|";
+	
 	private static final Instrumentation instance = new Instrumentation();
 	
-	private int numImpactedFlights = 0;
-	private double delay = 0;
-	
-	Aircraft aircraft = new Aircraft();
-	
-	
+	private FileWriter out = null;
 	
 	public static Instrumentation getInstance() {
 		return instance;
 	}
 	
 	private Instrumentation() {
-		File file = new File("output.csv");
+		try {
+			this.out = new FileWriter(this.buildFileName());
+			this.writeHeaderRow();
+		} catch (IOException e) {
+			
+		}
 	}
 	
-	// TODO
-			// will spit out into text file (pipes, tabs, comma...whatever delimited)
-			// the delays of each flight. should have year/month/day/hour/second of when the 
-			// class is first opened/created (singleton...only one instance globally for this class)
-			// static method getInstance...
-			// delay and the number of flights impacted are what will get pushed to text file (one entry per run)
-			// 
+	private String buildFileName() {
+		return new SimpleDateFormat("'output-'yyyyMMddHHmmss'.csv'").format(new Date());
+	}
 	
+	private void writeHeaderRow() throws IOException {
+		String[] columns = { "DELAY", "NUM_FLIGHTS_IMPACTED" };
+		writeRow(String.join(DELIMITER, columns));
+	}
+	
+	private void writeRow(String row) throws IOException {
+		out.write(row);
+		out.write('\n');
+	}
+
 	public void registerDelay(double delay, int numImpactedFlights) {
-		delay = aircraft.getDelay(); 
-		this.numImpactedFlights = numImpactedFlights;
+		try {
+			String[] values = {
+				Double.toString(delay),
+				Integer.toString(numImpactedFlights, 10)
+			};
+			writeRow(String.join(DELIMITER, values));
+		} catch (IOException ioe) {
+			
+		}
 	}
-	
-	
-	
 }

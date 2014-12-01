6cc53780f985ebcd7a831dcc89b24c38764b168a
Jim Blaney <jb38@hood.edu>
2014-11-30 22:44:12 -0500

    more fixing

--- src/main/java/edu/hood/cs/sim/Instrumentation.java
+++ src/main/java/edu/hood/cs/sim/Instrumentation.java
@@ -1,59 +1,55 @@
-// This class writes pipe-delimited output to a text file.
-// The content written comes from the Aircraft class, which
-// is used to generate delays at random for the flights, and 
-// then will give us a number for total impacted flights.
-
 package edu.hood.cs.sim;
 
-import edu.hood.cs.sim.domain.Aircraft;
 import java.io.FileWriter;
 import java.io.IOException;
-<<<<<<< HEAD
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+//TODO maybe create a domain POJO and store the values in the database
+
 public class Instrumentation {
 
-	// TODO maybe create a domain POJO and store the values in the database
-	
 	public static final String DELIMITER = "|";
 	
+	private static final Logger log = LogManager.getLogger(Instrumentation.class);
 	private static final Instrumentation instance = new Instrumentation();
-	
-	private FileWriter out = null;
-=======
-import java.io.Writer;
-
-public class Instrumentation {
-
-	private static final Instrumentation instance = new Instrumentation();
-	
-	private int numImpactedFlights = 0;
-	private double delay = 0;
-	
-	Aircraft aircraft = new Aircraft();
-	
-	
->>>>>>> 8d9b07c93594e6d8d2954f3444cd740f2655e683
 	
 	public static Instrumentation getInstance() {
 		return instance;
 	}
 	
+	private FileWriter out = null;
+	
 	private Instrumentation() {
-		
+		try {
+			this.out = new FileWriter(this.buildFileName());
+			this.writeHeaderRow();
+		} catch (IOException e) {
+			
+		}
 	}
 	
-	// TODO
-			// will spit out into text file (pipes, tabs, comma...whatever delimited)
-			// the delays of each flight. should have year/month/day/hour/second of when the 
-			// class is first opened/created (singleton...only one instance globally for this class)
-			// static method getInstance...
-			// delay and the number of flights impacted are what will get pushed to text file (one entry per run)
-			// 
+	private String buildFileName() {
+		return new SimpleDateFormat("'output-'yyyyMMddHHmmss'.csv'").format(new Date());
+	}
 	
+	private void writeHeaderRow() throws IOException {
+		String[] columns = { "DELAY", "NUM_FLIGHTS_IMPACTED" };
+		writeRow(String.join(DELIMITER, columns));
+	}
+	
+	private void writeRow(String row) throws IOException {
+		out.write(row);
+		out.write('\n');
+	}
+
 	public void registerDelay(double delay, int numImpactedFlights) {
-<<<<<<< HEAD
+		
+		log.info(String.format("Delay: %.0f Impacted: %d", delay, numImpactedFlights));
+		
 		try {
 			String[] values = {
 				Double.toString(delay),
@@ -63,12 +59,5 @@ public class Instrumentation {
 		} catch (IOException ioe) {
 			
 		}
-=======
-		delay = aircraft.getDelay(); 
-		this.numImpactedFlights = numImpactedFlights;
->>>>>>> 8d9b07c93594e6d8d2954f3444cd740f2655e683
 	}
-	
-	
-	
 }
